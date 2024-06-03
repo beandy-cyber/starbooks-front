e3d6d01eff6aae2091830377457dfd35b3bfffd7
Beatriz Andrade <Beatriz1andrade2013@gmail.com>
2024-06-03 00:39:42 -0300

    Update livros.js

--- src/servicos/livros.js
+++ src/servicos/livros.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const livrosAPI = axios.create({baseURL: "http://localhost:8000/livros"})
+const livrosAPI = axios.create({baseURL: "https://starbooks-server.onrender.com"})
 
 async function getLivros() {
     const response = await livrosAPI.get('/')
